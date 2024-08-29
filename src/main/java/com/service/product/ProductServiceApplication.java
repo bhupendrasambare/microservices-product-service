e41f8dd4ac625f6691b367588f7abab2bdc79627
bhupendrasambare <bhupendrasam1404@gmail.com>
2024-08-30 01:39:35 +0530

    Initial project

---
+++ src/main/java/com/service/product/ProductServiceApplication.java
@@ -0,0 +1,28 @@
+/**
+ * author @bhupendrasambare
+ * Date   :27/06/24
+ * Time   :5:01 pm
+ * Project:microservices-registry
+ **/
+package com.service.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class ProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceApplication.class, args);
+	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
+
+}
---
+++ src/main/java/com/service/product/config/Constants.java
@@ -0,0 +1,21 @@
+/**
+ * author @bhupendrasambare
+ * Date   :27/06/24
+ * Time   :5:01 pm
+ * Project:microservices-registry
+ **/
+package com.service.product.config;
+
+public class Constants {
+
+    public static final String SUCCESS_CODE="0";
+    public static final String USER_NOT_FOUND_CODE="20001";
+    public static final String INVALID_PASSWORD_CODE="20002";
+    public static final String INVALID_TOKEN_CODE = "20003";
+    public static final String INTERNAL_SERVER_ERROR_CODE = "10001";
+
+    public static final String SUCCESS="SUCCESS";
+    public static final String USER_NOT_FOUND = "User not found";
+    public static final String INVALID_PASSWORD = "Invalid password";
+    public static final String INVALID_TOKEN = "Invalid token";
+}
